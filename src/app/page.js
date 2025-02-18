9862d7a4a4b8621b80bbbc7e6122fa0d13dec305
Guillaume Sere <guillaumesere@hotmail.fr>
2025-02-18 14:33:15 +0100

    modif css

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import GameList from './components/GameList';
 import Header from './components/header';
+import Description from './components/Description';
 
 export default function Home() {
     return (
         <>
             <Header />
             <div className='container mx-auto px-4 py-8'>
+                <Description />
                 <GameList />
             </div>
         </>
