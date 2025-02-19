b531ed8d2e562951305409234ed221a10d8b5f5d
Guillaume Sere <guillaumesere@hotmail.fr>
2025-02-19 10:20:19 +0100

    add message erreurs

--- src/app/components/GameList.js
+++ src/app/components/GameList.js
@@ -2,81 +2,86 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const GameList = () => {
-  const [searchTerm, setSearchTerm] = useState(''); // champ de recherche pour le terme
-  const [objectData, setObjectData] = useState(null);
+    const [searchTerm, setSearchTerm] = useState(''); // champ de recherche pour le terme
+    const [objectData, setObjectData] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(''); // message d'erreur pour l'objectId inexistant
 
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${searchTerm}`);
-      if (response.data.objectIDs.length > 0) {
-        const objectId = response.data.objectIDs[0]; // On prend le premier objet ID trouvé
-        const detailResponse = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`);
-        setObjectData(detailResponse.data);
-        console.log(detailResponse.data);
-      } else {
-        console.log('Aucun objet trouvé avec ce terme de recherche.');
-      }
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données:', error);
-    }
-  };
+    const handleSearch = async () => {
+        setErrorMessage(''); // Réinitialisation du message d'erreur avant chaque recherche
+        try {
+            const response = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${searchTerm}`);
+            if (response.data.objectIDs.length > 0) {
+                const objectId = response.data.objectIDs[0]; // On prend le premier objet ID trouvé
+                const detailResponse = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`);
+                setObjectData(detailResponse.data);
+                console.log(detailResponse.data);
+            } else {
+                console.log('Aucun objet trouvé avec ce terme de recherche.');
+                setErrorMessage('Aucun objet trouvé avec ce terme de recherche. Veuillez essayer une nouvelle recherche.'); // Message d'erreur si aucun objet trouvé
+            }
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données:', error);
+            setErrorMessage('Erreur lors de la récupération des données. Veuillez essayer une nouvelle recherche.'); // Message d'erreur en cas d'erreur de récupération
+        }
+    };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleSearch();
-    }
-  };
-
-  return (
-    <div className='description max-w-2xl mx-auto'>
-      <h2 className="text-lg font-bold text-black">Recherche d'Oeuvres d'art (entrez un terme comme Rome, Louis ou un numéro)</h2>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Entrer un terme de recherche"
-        className="w-full p-2 mb-4 border border-gray-400 text-black"
-        onKeyDown={handleKeyDown}
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded"
-      >
-        Rechercher
-      </button>
-      {objectData ? (
-        <div className='description'>
-          {objectData.primaryImage ? (
-            <a href={objectData.primaryImage} target="_blank" rel="noopener noreferrer">
-              <img src={objectData.primaryImage} alt="Image principale" className="max-w-2xs mb-4" />
-            </a>
-          ) : (
-            <p className="text-lg font-semibold">Aucune image principale disponible pour cet objet ID</p>
-          )}
-          <p className="text-lg font-semibold">{objectData.title}</p>
-          <p className="text-sm text-black">{objectData.artistDisplayName}</p>
-          <p className="text-sm text-black">{objectData.country}</p>
-          <p className="text-sm text-black">{objectData.creditLine}</p>
-          <p className="text-sm text-black">{objectData.culture}</p>
-          <p className="text-sm text-black">{objectData.department}</p>
-         <a href={objectData.artistWikidata_URL} target="_blank" className='text-green-500'>En savoir plus sur l'artiste</a>
-          {objectData.additionalImages && objectData.additionalImages.length > 0 ? (
-            <div className="flex flex-wrap">
-              {objectData.additionalImages.map((image, index) => (
-                <a key={index} href={image} target="_blank" rel="noopener noreferrer">
-                  <img src={image} alt={`Image secondaire ${index + 1}`} className="max-w-2xs mb-4 mt-5 mr-2" />
-                </a>
-              ))}
-            </div>
-          ) : (
-            <p className="text-red-500">Aucune image secondaire disponible pour cet objet ID</p>
-          )}
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
+    return (
+        <div className='description max-w-2xl mx-auto'>
+            <h2 className="text-lg font-bold text-black">Recherche d'Oeuvres d'art (entrez un terme comme Rome, Louis ou un numéro)</h2>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Entrer un terme de recherche (minimum 3 caractères)"
+                className="w-full p-2 mb-4 border border-gray-400 text-black"
+                onKeyDown={handleKeyDown}
+                minLength={3}
+            />
+            {searchTerm.length < 3 && <p className="text-red-500 text-sm mb-4">Veuillez entrer au moins 3 caractères.</p>}
+            <button
+                onClick={handleSearch}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded"
+                disabled={searchTerm.length < 3}
+            >
+                Rechercher
+            </button>
+            {errorMessage && <p className="text-red-500 text-sm mb-4">{errorMessage}</p>} 
+            {objectData ? (
+                <div className='description'>
+                    {objectData.primaryImage ? (
+                        <a href={objectData.primaryImage} target="_blank" rel="noopener noreferrer">
+                            <img src={objectData.primaryImage} alt="Image principale" className="max-w-2xs mb-4" />
+                        </a>
+                    ) : (
+                        <p className="text-lg font-semibold text-red-500">Aucune image principale disponible pour cet objet ID</p>
+                    )}
+                    <p className="text-lg font-semibold">{objectData.title}</p>
+                    <p className="text-sm text-black">{objectData.artistDisplayName}</p>
+                    <p className="text-sm text-black">{objectData.country}</p>
+                    <p className="text-sm text-black">{objectData.creditLine}</p>
+                    <p className="text-sm text-black">{objectData.culture}</p>
+                    <p className="text-sm text-black">{objectData.department}</p>
+                    <a href={objectData.artistWikidata_URL} target="_blank" className='text-green-500'>En savoir plus sur l'artiste</a>
+                    {objectData.additionalImages && objectData.additionalImages.length > 0 ? (
+                        <div className="flex flex-wrap">
+                            {objectData.additionalImages.map((image, index) => (
+                                <a key={index} href={image} target="_blank" rel="noopener noreferrer">
+                                    <img src={image} alt={`Image secondaire ${index + 1}`} className="max-w-2xs mb-4 mt-5 mr-2" />
+                                </a>
+                            ))}
+                        </div>
+                    ) : (
+                        <p className="text-red-500">Aucune image secondaire disponible pour cet objet ID</p>
+                    )}
+                </div>
+            ) : null}
         </div>
-      ) : (
-        <p className="text-red-500">Aucun objet trouvé avec ce terme de recherche.</p>
-      )}
-    </div>
-  );
+    );
 };
 
 export default GameList;
